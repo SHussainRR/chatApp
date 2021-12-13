abc93590dce0635fa827980ca5df60c5e42df40c
Hussain Raza <hussain.raza@logicielservice.com>
2021-12-13 13:39:19 +0500

    Navigation Button Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AddgroupComponent } from './addgroup/addgroup.component';
 import { GrouproomComponent } from './grouproom/grouproom.component';
 import { OneChatroomComponent } from './one-chatroom/one-chatroom.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login-routing.module').then(m => m.LoginRoutingModule) },
   { path: 'roomlist', component: RoomlistComponent },
   { path: 'addroom', component: AddroomComponent },
+  { path: 'addgroup' , component:AddgroupComponent},
   { path: 'addone', component: AddoneComponent},
   { path: 'chatroomone/:roomname' , component: OneChatroomComponent},
   { path: 'chatroom/:roomname', component: ChatroomComponent },
