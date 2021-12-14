3010f654bb42c12839261b40f028efdea44cd0c9
Hussain Raza <hussain.raza@logicielservice.com>
2021-12-14 18:19:36 +0500

    empty Field bug fixed

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,8 +3,11 @@ import * as firebase from "firebase";
 export const isLoggedIn = () => getUserId() && true;
 
 export const makeUserOnline = (online) => {
+  if(localStorage.getItem('userId') !== null){
+    
     const userRef = firebase.database().ref('users/' + localStorage.getItem('userId'));
     userRef.update({status: online? "online" : "ofline" });
+  }
 }
 
 export const snapshotToArray = (snapshot: any) => {
